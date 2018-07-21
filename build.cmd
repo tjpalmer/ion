0fa2afd97a28c4dafffbd774b27551212de58be1
Tom <tjpalmer@tjpalmer.com>
2018-07-21 09:13:12 -0700

    Bootstrap msvc (just 2017 for now) if needed

--- build.cmd
+++ build.cmd
@@ -5,9 +5,11 @@ set IONHOME=%~dp0
 set IONOS=win32
 
 pushd %IONHOME%
-if not exist bin (
-  mkdir bin
-)
+
+pushd .
+call bin\vsenv
+popd
+
 if exist bin\ion.exe (
   bin\ion.exe -o out\out_ion_win32.c ion
 ) else (
@@ -15,7 +17,9 @@ if exist bin\ion.exe (
   echo -- No ion binary found; using previously built out/out_ion_win32.c --
   echo.
 )
+
 cl ion.c /Fe:bin\ion /Fo:bin\ion
+
 popd
 
 endlocal
